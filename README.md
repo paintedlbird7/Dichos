bcbd8002e19b1cd760fdecdb49cf88144a52a8d9
Rosa M Perez <rosaperezlarios@gmail.com>
2025-03-14 16:09:17 -0700

    wip(schema): completed applications schema

---
+++ README.md
@@ -0,0 +1,13 @@
+## Application Routes
+
+| Action  | Route | HTTP Verb |
+|---------|--------------------------------------|-----------|
+| Index   | `/users/:userId/dichos`       | GET       |
+| New     | `/users/:userId/dichos/new`   | GET       |
+| Create  | `/users/:userId/dichos`       | POST      |
+| Show    | `/users/:userId/dichos/:dichoId` | GET   |
+| Edit    | `/users/:userId/dichos/:dichoId/edit` | GET  |
+| Update  | `/users/:userId/dichos/:dichoId` | PUT   |
+| Delete  | `/users/:userId/dichos/:dichoId` | DELETE |
+
+![Alt text](assets/dichos.png)
--- models/user.js
+++ models/user.js
@@ -1,4 +1,26 @@
 const mongoose = require('mongoose');
+// TODO: find out how to transfer master into the main branch
+
+const dichoSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['english', 'spanish'],
+  },
+});
 
 const userSchema = mongoose.Schema({
   username: {
@@ -9,6 +31,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  dichos: [dichoSchema], // Embedded schema as an array
 });
 
 const User = mongoose.model('User', userSchema);
