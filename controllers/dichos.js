0594448cb3da8990f7ab76da792331aa42a0dbc9
Rosa M Perez <rosaperezlarios@gmail.com>
2025-03-14 23:36:18 -0700

    wip: render the applications index template

--- controllers/dichos.js
+++ controllers/dichos.js
@@ -6,7 +6,13 @@ const router = express.Router();
 const User = require('../models/user.js');
 
 router.get('/', (req, res) => {
-    res.send('Hello dichos index route!');
+    // res.send('Hello dichos index route!');
+    try {
+        res.render('dichos/index.ejs');
+      } catch (error) {
+        console.log(error);
+        res.redirect('/');
+      }
   });
 
 module.exports = router;
