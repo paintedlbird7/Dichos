0290702da7c7ba3bd4b26868c94740170da381ff
Rosa M Perez <rosaperezlarios@gmail.com>
2025-03-15 12:10:00 -0700

    wip: data was successfully saved &  redirected to thedichos page

--- controllers/dichos.js
+++ controllers/dichos.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-// TODO: left off on Defining the route on the server
+// TODO: left off Defining the route and coding the controller
 
 const User = require('../models/user.js');
 
@@ -21,5 +21,27 @@ router.get('/', (req, res) => {
 router.get('/new', async (req, res) => {
     res.render('dichos/new.ejs');
   });
+
+
+  // controllers/applications.js`
+// POST form
+router.post('/', async (req, res) => {
+    try {
+      // Look up the user from req.session
+      const currentUser = await User.findById(req.session.user._id);
+      // Push req.body (the new form data object) to the
+      // applications array of the current user
+      currentUser.dichos.push(req.body);
+      // Save changes to the user
+      await currentUser.save();
+      // Redirect back to the applications index view
+      res.redirect(`/users/${currentUser._id}/dichos`);
+    } catch (error) {
+      // If any errors, log them and redirect back home
+      console.log(error);
+      res.redirect('/');
+    }
+  });
+  
   
 module.exports = router;
