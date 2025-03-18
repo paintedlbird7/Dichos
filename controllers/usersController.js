faf4ee328d098661df72ade8d2effb8b5bd8b9fd
Rosa M Perez <rosaperezlarios@gmail.com>
2025-03-18 15:59:05 -0700

    wip feat: gets all users

---
+++ controllers/usersController.js
@@ -0,0 +1,14 @@
+const User = require("../models/user"); 
+
+// GET all users
+async function getAllUsers(req, res) {
+  try {
+    const users = await User.find({}, "username");
+    res.render("users/index", { users });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+}
+
+module.exports = { getAllUsers };
